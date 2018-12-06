28048b13f51264ba5a7ea065f1213b20101543a9
Dipang JS <dipang.sheth88@gmail.com>
2018-12-06 19:18:08 +0530

    modified files

---
+++ SimpleView/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SimpleView
+//
+//  Created by Dipang Sheth on 06/12/18.
+//  Copyright © 2018 Erdos Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
